80d33ea445716d30fc75ead9cbf4ae96ac146706
Dmitry <Dmitry.Mosin@urfu.me>
2022-09-22 19:38:59 +0500

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -3,24 +3,17 @@ function getRandomNumber(min, max)
   if (min > max) {
     [min, max] = [max, min];
   }
-        
+    
   if (min < 0) {
     throw {name: 'incorrectValues', message: 'Only positive values'};
   }
-        
+
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 getRandomNumber(24,45);
 
 
-/**
- * Функция сравнивает длину строки с передаваемым значением
- * 
- * @param {*} str - проверяемая строка
- * @param {*} length - ограничение длины строки
- * @returns 
- */
 const checkLengthOfString = (str, length=140) => str.length <= length;
 
 checkLengthOfString('Какая-то случайная строка', 90);
